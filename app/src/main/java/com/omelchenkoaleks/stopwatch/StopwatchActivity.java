7e7bae366c03ce680a6a89f82455375147815dfb
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-02 20:58:32 +0300

    создан метод, который будет отсчитывать секундомер

--- app/src/main/java/com/omelchenkoaleks/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/omelchenkoaleks/stopwatch/StopwatchActivity.java
@@ -2,7 +2,11 @@ package com.omelchenkoaleks.stopwatch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class StopwatchActivity extends Activity {
 
@@ -15,6 +19,8 @@ public class StopwatchActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+
+        runTimer();
     }
 
     public void onClickStart(View view) {
@@ -30,5 +36,28 @@ public class StopwatchActivity extends Activity {
         running = false;
         // обнуляем секундомер
         seconds = 0;
+    }
+
+    // чтобы обновить секундомер планируем многократное выполнение кода с использованием Handler
+    private void runTimer() {
+        final TextView timeView = findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        // вызов метода post() обеспечивает выполнение без задержки
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format(Locale.getDefault(), "%d:%02:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                // код передается на повторное исполнение через 1 секунду
+                handler.postDelayed(this, 1000);
+            }
+        });
+
     }
 }
