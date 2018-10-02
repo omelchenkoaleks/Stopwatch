36027fcb7b49aaf738c6ccfa100d6164e922d675
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-02 20:32:55 +0300

    построен макет и реализованы методы старт, стоп и сброс секундомера

--- app/src/main/java/com/omelchenkoaleks/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/omelchenkoaleks/stopwatch/StopwatchActivity.java
@@ -2,12 +2,33 @@ package com.omelchenkoaleks.stopwatch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StopwatchActivity extends Activity {
+
+    // хранится текущее количество секунд
+    private int seconds = 0;
+    // хранит значение работает ли секундомер в настоящий момент = Флаг работы
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+    }
+
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    public void onClickStop(View view) {
+        running = false;
+    }
+
+    public void onClickReset(View view) {
+        // останавливаем секундомер
+        running = false;
+        // обнуляем секундомер
+        seconds = 0;
     }
 }
